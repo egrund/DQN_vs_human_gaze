e071447e2bad474d0e6963d9461996ce51eb97a0
egrund <82370915+egrund@users.noreply.github.com>
2022-08-06 14:22:51 +0200

    preliminary ig code and test

--- saliency/heatmap_comparison.py
+++ saliency/heatmap_comparison.py
@@ -1,6 +1,7 @@
 from sklearn.metrics import roc_auc_score
 import tensorflow as tf
 import numpy as np
+from scipy import ndimage as ndi 
 
 def heatmap_comparison_using_AUC(map1, map2):
     """ binary classifier: Area Under ROC Curve """
@@ -11,8 +12,8 @@ def heatmap_comparison_using_AUC(map1, map2):
     map1_normal = map1 / map1_max
     map2_normal = map2 / map2_max
     # make map1 only 0 or 1 
-    map1_rounded = round_with_threshold(map1_normal,0.2)
-    map2_rounded = round_with_threshold(map2_normal,0.2)
+    map1_rounded = round_with_threshold(map1_normal)
+    map2_rounded = round_with_threshold(map2_normal)
     # flatten both
     map1_flat = tf.reshape(map1_rounded,[-1]).numpy()
     map2_flat = tf.reshape(map2_rounded,[-1]).numpy()
@@ -21,5 +22,35 @@ def heatmap_comparison_using_AUC(map1, map2):
 
     return auc_score, map1_rounded, map2_rounded
 
-def round_with_threshold(array,threshold=0.5, min=0, max=1):
+def round_with_threshold(array,threshold=0.2, min=0, max=1):
     return np.where(array > threshold, max, min)
+
+def saliency_information_gain(fixation_map, saliency,epsilon=0.1):
+    """ compare by using Information Gain 
+    
+    Arguments: 
+        fixation_map (2D array or Tensor): binary map of fixation points (not a heatmap) 
+        saliency: saliency heatmap
+        epsilon (float): for regularization
+    """
+    center_prior = create_center_prior_baseline(fixation_map)
+
+    log_saliency = np.log2(saliency + epsilon)
+    log_prior = np.log2(center_prior + epsilon)
+    n = tf.reshape(fixation_map,[-1]).shape[0]
+
+    # calculation
+    information_gain = ( 1/n ) * tf.reduce_sum(tf.multiply(tf.squeeze(fixation_map,axis=-1),log_saliency - log_prior))
+    return information_gain.numpy()
+    
+def create_center_prior_baseline(map):
+    """ creates a center prior baseline in the shape of map """
+    base = np.zeros(shape=map.shape)
+    center_x = base.shape[0] // 2
+    center_y = base.shape[1] //2
+    base[center_x,center_y] = 1
+    base = ndi.gaussian_filter(base, sigma = 10) # compare my_reader_class.create_gaze_heatmap
+    return base
+
+
+
--- saliency/test_heatmap_comparison_auc.py
+++ saliency/test_heatmap_comparison_auc.py
@@ -5,10 +5,8 @@ from sample_trajectory import preprocess_image
 from dqn import DQN
 from sarfa_saliency import computeSaliencyUsingSarfa
 
-
 import matplotlib.pyplot as plt
 import tensorflow as tf
-import numpy as np
 
 data = Reader()
 
@@ -31,7 +29,6 @@ plt.show()
 # compare human gaze heatmap with sarfa saliency heatmap
 # check perturbation_sarfa_example.py
 I = 500
-FRAME_SKIPS = 4
 MODE = 'blurred'
 heatmap = data.create_gaze_heatmap(I)
 model = DQN(9)
@@ -39,18 +36,8 @@ model(tf.random.uniform(shape=(1,84,84,4)),training = False)
 #model.load_weights() # add path
 original_image = tf.convert_to_tensor(data.get_image(I))
 image = preprocess_image(original_image,84,84)
-observation = tf.repeat(image,FRAME_SKIPS,axis=-1) # model gets several times the same image
-q_vals = tf.squeeze(model(tf.expand_dims(observation,axis=0),training = False),axis=0)
-action = tf.argmax(q_vals).numpy()
-masks = pert.create_masks(image) # one mask for every pixel
-saliency = np.zeros(shape=(len(masks)))
-for i,mask in enumerate(masks):
-    p_image = tf.convert_to_tensor(pert.perturb_image(image.numpy(),mask, mode=MODE))
-    observation = tf.repeat(p_image,FRAME_SKIPS,axis=-1) # model gets several times the same image
-    p_q_vals = tf.squeeze(model(tf.expand_dims(observation,axis=0),training = False),axis = 0)
-    sal,_,_,_,_,_ = computeSaliencyUsingSarfa(action,pert.array_to_dict(q_vals.numpy()),pert.array_to_dict(p_q_vals.numpy()))
-    saliency[i] = sal
-saliency = pert.image_to_size(tf.reshape(saliency,shape=image.shape))
+saliency, perturbed_image = pert.calc_saliency_for_image(image,model,mode=MODE)
+saliency = pert.image_to_size(saliency)
 
 auc, map1, map2 = compare.heatmap_comparison_using_AUC(heatmap, saliency)
 
