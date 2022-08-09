fixed overflow

saliency/perturbation_for_sarfa.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy import ndimage as ndi 
 import tensorflow as tf
 
-def create_masks(image,sigma=10):
+def create_masks(image,sigma=5):
     """creates an image with a mask around point (x,y) with radius sigma 
     
     Arguments: 
@@ -32,7 +32,7 @@ def perturb_image(image,mask,mode='blurred',perturbation = None):
     """
 
     shape = image.shape
-    if(mode != 'image'):
+    if(mode != 'image'): # blurred default
         perturbation = ndi.gaussian_filter(image, sigma=5) # blurred
     if(mode == 'black'):
         perturbation = tf.zeros(shape=shape)
@@ -64,7 +64,7 @@ def image_to_size(image,y=160,x=210):
     image = tf.image.resize(image,size=(x,y))
     return image
 
-def calc_sarfa_saliency_for_image(image, model, mode = 'blurred',sigma = 8, frame_skips = 4):
+def calc_sarfa_saliency_for_image(image, model, mode = 'blurred',sigma = 5, frame_skips = 4):
     """ calculates the saliency for an whole image 
     
     Arguments: 
@@ -72,7 +72,7 @@ def calc_sarfa_saliency_for_image(image, model, mode = 'blurred',sigma = 8, fram
         model: the NN to predict the values
         mode (String): mode of perturbation see perturb_image
         sigma
-        frame_skip (int): how many frames the model gets at once
+        frame_skips (int): how many frames the model gets at once
     """
 
     observation = tf.repeat(image,frame_skips,axis=-1) # model gets several times the same image
@@ -90,7 +90,7 @@ def calc_sarfa_saliency_for_image(image, model, mode = 'blurred',sigma = 8, fram
         observation = tf.repeat(p_image,frame_skips,axis=-1) # model gets several times the same image
 
         p_q_vals = tf.squeeze(model(tf.expand_dims(observation,axis=0),training = False),axis = 0)
-        sal,_,_,_,_,_ = computeSaliencyUsingSarfa(action,array_to_dict(q_vals.numpy()),array_to_dict(p_q_vals.numpy()))
+        sal,_,_,_,_,_ = computeSaliencyUsingSarfa(action,array_to_dict(q_vals),array_to_dict(p_q_vals))
         saliency[i] = sal
 
     saliency = tf.reshape(saliency,shape=image.shape)

saliency/perturbation_sarfa_example.py
@@ -8,14 +8,14 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 FRAME_SKIPS = 4
-I = 5 # index of frame (1 to data.get_number_frames())
-MODE = 'blurred' # 'black', 'white', 'random'
-SIGMA = 8 # size of perturbation
+I = 250 # index of frame (1 to data.get_number_frames())
+MODE = 'black' #'blurred' # 'black', 'white', 'random'
+SIGMA = 5 # size of perturbation
 
 data = Reader() #file_dir = , images_dir = ) # add path of txt file and 
 model = dqn.model(9)
 model(tf.random.uniform(shape=(1,84,84,4)),training = False)
-#model.load_weights('asterix_test/run2/model') # add path
+model.load_weights('asterix_test/run2/model') # add path
 
 original_image = tf.convert_to_tensor(data.get_image(I))
 image = preprocess_image(tf.convert_to_tensor(original_image),84,84)

saliency/sarfa_saliency.py
@@ -44,15 +44,15 @@ def computeSaliencyUsingSarfa(original_action, dict_q_vals_before_perturbation, 
     answer = 0
 
     # probability of original move in perturbed state
-    # print(dict_q_vals_after_perturbation)
     q_value_action_perturbed_state = dict_q_vals_after_perturbation[original_action]
     q_value_action_original_state = dict_q_vals_before_perturbation[original_action]
 
-    q_values_after_perturbation = np.asarray(list(dict_q_vals_after_perturbation.values()))
-    q_values_before_perturbation = np.asarray(list(dict_q_vals_before_perturbation.values()))
+    q_values_after_perturbation = np.asarray(list(dict_q_vals_after_perturbation.values())) 
+    q_values_before_perturbation = np.asarray(list(dict_q_vals_before_perturbation.values())) 
     
-    probability_action_perturbed_state = np.exp(q_value_action_perturbed_state) / np.sum(np.exp(q_values_after_perturbation))
-    probability_action_original_state = np.exp(q_value_action_original_state) / np.sum(np.exp(q_values_before_perturbation))
+    # I changed the next two lines, because with our q-values they overflow. The result is exactly the same
+    probability_action_perturbed_state = your_softmax(q_values_after_perturbation)[original_action] #np.exp(q_value_action_perturbed_state) / np.sum(np.exp(q_values_after_perturbation))
+    probability_action_original_state = your_softmax(q_values_before_perturbation)[original_action] #np.exp(q_value_action_original_state) / np.sum(np.exp(q_values_before_perturbation))
     
     K = cross_entropy(dict_q_vals_after_perturbation, dict_q_vals_before_perturbation, original_action)
 
@@ -60,7 +60,6 @@ def computeSaliencyUsingSarfa(original_action, dict_q_vals_before_perturbation, 
 
     if probability_action_perturbed_state < probability_action_original_state: # harmonic mean
         answer = 2*dP*K/(dP + K)     
-        
     
     QmaxAnswer = computeSaliencyUsingQMaxChange(original_action, dict_q_vals_before_perturbation, dict_q_vals_after_perturbation)
     action_gap_before_perturbation, action_gap_after_perturbation = computeSaliencyUsingActionGap(dict_q_vals_before_perturbation, dict_q_vals_after_perturbation)

saliency/use_integrated_gradients.py
@@ -10,12 +10,12 @@ import matplotlib.pyplot as plt
 FRAME_SKIPS = 4
 MODE = 'Black'
 M_STEPS = 10
-I = 500 # index of frame (1 to data.get_number_frames())
+I = 200 # index of frame (1 to data.get_number_frames())
 
 data = Reader() #file_dir = , images_dir = ) # add path of txt file and 
 model = dqn.model(9)
 model(tf.random.uniform(shape=(1,84,84,4)))
-#model.load_weights('asterix_test/run2/model') # add path
+model.load_weights('asterix_test/run2/model') # add path
 
 image = preprocess_image(tf.convert_to_tensor(data.get_image(I)),84,84)
 observation = tf.repeat(image,FRAME_SKIPS,axis=-1) # model gets several times the same image
@@ -57,7 +57,7 @@ axs[1,0].axis('off')
 
 axs[1,1].set_title('Original + IG Attribution Mask Overlay')
 axs[1,1].imshow(attribution_mask, cmap=plt.cm.inferno)
-axs[1,1].imshow(image, cmap = 'gray', alpha=0.4)
+axs[1,1].imshow(image, cmap = 'gray', alpha=0.2)
 axs[1,1].axis('off')
 
 plt.tight_layout()