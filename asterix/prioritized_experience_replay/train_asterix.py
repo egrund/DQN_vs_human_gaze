8d2ab6082cdfbdaa5fe89c6cb9a8cbbf805de73f
leonl42 <55513915+leonl42@users.noreply.github.com>
2022-09-06 13:19:08 +0200

    Update train_asterix.py

--- asterix/prioritized_experience_replay/train_asterix.py
+++ asterix/prioritized_experience_replay/train_asterix.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     USE_PRIORITIZED_REPLAY = False # should prioritized replay be used
     EPSILON_DECAY = 0.004 # how much to decay epsilon each iteration
     INNER_ITS = 40 # how many training steps per iteration
-    SAMPLES_FROM_ENV = 2000 # minimum of how many new samples from the environment should be added to the buffer each iteration
+    SAMPLES_FROM_ENV = 2000 # how many new samples from the environment should be added to the buffer each iteration (in expectation)
     TRAIN_ITS = 12000 # how many training iterations should be done
     INITIAL_EPSILON = 1 # initial value of epsilon
     EPSILON_MIN = 0.1 # minimum value of epsilon that can be reached
@@ -21,8 +21,8 @@ if __name__ == '__main__':
     POLYAK_UPDATE = 0.000125 # polyak update for each training step (so INNER_ITS polyak updates per training iteration)
     OPTIMIZER = tf.keras.optimizers.Adam(0.0025, beta_1=0.9, beta_2=0.999, epsilon=1e-07) # optimizer that is used
     ENV = "ALE/Asterix-v5" # environment name
-    LOG_PATH_WEIGHTS = 'asterix_test/run30' # where to store the weights
-    LOG_PATH_TENSORBOARD = 'logs/asterix_test/run30/' # where to store dqn loss and reward for tensorboard
+    LOG_PATH_WEIGHTS = 'asterix_test/run1/' # where to store the weights
+    LOG_PATH_TENSORBOARD = 'logs/asterix_test/run1/' # where to store dqn loss and reward for tensorboard
 
     # initialize buffer
     with tf.device("/CPU:0"):
