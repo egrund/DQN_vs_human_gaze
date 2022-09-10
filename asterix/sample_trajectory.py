30d19a20ca2a33290fe0fdb5f48129e93c9a8a1d
leonl42 <55513915+leonl42@users.noreply.github.com>
2022-09-10 11:46:08 +0200

    Update sample_trajectory.py

--- asterix/sample_trajectory.py
+++ asterix/sample_trajectory.py
@@ -1,21 +1,14 @@
 import gym
 import random as rand
-import time 
-import joblib
+import tensorflow as tf
 import numpy as np
 
 def preprocess_image(image, imgx,imgy):
-    import tensorflow as tf
 
-    with tf.device("/CPU:0"):
-
-        image = tf.image.resize(image, (imgx,imgy))
-
-    
+    image = tf.image.resize(image, (imgx,imgy))
     return image
 
 def sample_from_env(env : gym.Env, action : int, imgx : int, imgy : int, stack_frames : int, noops : int):
-    import tensorflow as tf
     
     observation, reward, done,_trunc, _ = env.step(action)
     observation = preprocess_image(observation, imgx, imgy)
@@ -27,73 +20,89 @@ def sample_from_env(env : gym.Env, action : int, imgx : int, imgy : int, stack_f
         if done == True:
 
             if i < stack_frames-1:
-                with tf.device("/CPU:0"):
-                    new_observation = tf.repeat(tf.expand_dims(observation[:,:,-1],axis=-1),(stack_frames-1-i)*3,axis=-1)
-                    observation = tf.concat([observation,new_observation],axis=-1)
+
+                new_observation = tf.repeat(tf.expand_dims(observation[:,:,-1],axis=-1),(stack_frames-1-i)*3,axis=-1)
+                observation = tf.concat([observation,new_observation],axis=-1)
             break
 
         new_observation, new_reward, done, _trunc, _  = env.step(noops)
         new_observation = preprocess_image(new_observation, imgx, imgy)
 
 
-        with tf.device("/CPU:0"):
-            observation = tf.concat([observation,new_observation],axis=-1)
+
+        observation = tf.concat([observation,new_observation],axis=-1)
         reward += new_reward
 
     
     return observation,reward,done
 
 
+def create_trajectory(model, batch : int, epsilon : float,env_name : str ,frame_skips : int,imgx : int, imgy : int):
 
-def create_trajectory(model, epsilon : float,env_name : str ,frame_skips : int,imgx : int, imgy : int, its : int):
-    import tensorflow as tf
-
-    start_of_function = time.time()
 
     s_a_r_s = []
-    env = gym.make(env_name,full_action_space=False,new_step_api=True)
-    #env = AtariPreprocessing(env = env)
 
     # create action space
-    ACTION_SPACE_SIZE = env.action_space.n # only discrete
+    ACTION_SPACE_SIZE = gym.make(env_name,full_action_space=False,new_step_api=True).action_space.n # only discrete
 
-    observation = env.reset()
-    observation = preprocess_image(observation, imgx, imgy)
-    # bring start state into format of all other states
-    with tf.device("/CPU:0"):
-        observation = tf.repeat(observation,frame_skips,axis=-1)
+    #  create number of environments equal to batch size
+    envs = [gym.make(env_name,full_action_space=False,new_step_api=True) for _ in range(batch)]
 
-    for k in range(10000):
+    # create starting frame for each environment
+    observations = [preprocess_image(env.reset(),imgx, imgy) for env in envs]
+    observations = [tf.repeat(observation,frame_skips,axis=-1) for observation in observations]
+
+
+
+    for _ in range(10000):
         
-        loop_beginning = time.time()
 
-        # epsilon greedy policy
-        if rand.randint(0,100)<=epsilon*100:
-            action = rand.randint(0,ACTION_SPACE_SIZE-1)
-        else:
-            with tf.device("/GPU:0"):
-                action = tf.argmax(tf.squeeze(model(tf.expand_dims(observation,axis=0),training = False),axis = 0)).numpy().astype(np.int16)
-        
-        # sample from environment
-        new_observation, reward, done = sample_from_env(env, action,imgx , imgy, stack_frames=frame_skips, noops = 0)
+        if not len(observations) == len(envs):
+            raise Exception("Length of lists 'observation' and 'envs' did not match")
 
-        s_a_r_s.append((observation.numpy().astype(np.int16),action,reward,new_observation.numpy().astype(np.int16), done))
-        observation = new_observation
+        # create list that determines which actions will be sampled and which will be done with the model
+        action_selector = [rand.randint(0,100)<=epsilon*100 for _ in range(len(envs))]
+
+        # to avoid tf.function raytracing, ensure that always a tensor of the same shape is passed to the model
+        while len(observations) < batch:
+            observations.append(tf.zeros(tf.shape(observations[0])))
+
+
+        # calculate the actions given the model using a batch
+        with tf.device("/GPU:0"):
+
+            actions = tf.argmax(model(tf.stack(observations,axis = 0),training = False), axis = -1).numpy().astype(np.int16)
+
+
+        observations = observations[:len(envs)]
+        actions = actions[:len(envs)]
+
+        # sample new actions if the action selector says so
+        actions = [rand.randint(0,ACTION_SPACE_SIZE-1) if should_sample_action else action for should_sample_action,action in zip(action_selector,actions)]
+
+        result_from_env = []
+        for env,action in zip(envs,actions):
+            observation,reward,done = sample_from_env(env, action, imgx , imgy, stack_frames = frame_skips, noops = 0)
+            result_from_env.append((observation,reward,done))
+
+        done_and_remove_index = []
+        for i, elem in enumerate(zip(observations,actions,result_from_env)):
+            observation,action,(new_observation, reward, done) = elem
+            s_a_r_s.append((observation.numpy().astype(np.int16),action,reward,new_observation.numpy().astype(np.int16), done))
+            observations[i] = new_observation
+            if done:
+                done_and_remove_index.append(i)
             
+        envs = [env for i,env in enumerate(envs) if not i in done_and_remove_index]
+        observations = [observation for i,observation in enumerate(observations) if not i in done_and_remove_index]
 
-        if done:
-            if k < its:
-                observation = env.reset()
-                observation = preprocess_image(observation, imgx, imgy)
-                # bring start state into format of all other states
-                with tf.device("/CPU:0"):
-                    observation = tf.repeat(observation,frame_skips,axis=-1)
-            else:
-                break
-            
+
+        if len(envs) == 0:
+            break
+    
+    
     
     return s_a_r_s
-
 
 
 
