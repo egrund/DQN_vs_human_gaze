09313a68c478e9e83fe30f0af1afb82a85311c77
leonl42 <55513915+leonl42@users.noreply.github.com>
2022-09-10 11:46:19 +0200

    Update train_asterix.py

--- asterix/train_asterix.py
+++ asterix/train_asterix.py
@@ -5,7 +5,8 @@ from buffer import Buffer
 from model import AgentModel
 import socket
 import pickle
-
+import numpy as np
+import matplotlib.pyplot as plt
 from sample_trajectory import create_trajectory
 
 if __name__ == '__main__':
@@ -13,24 +14,24 @@ if __name__ == '__main__':
 
     # set hyperparameters
     params = {
-    "BUFFER_SIZE" : 500000, # max size of buffer
-    "BUFFER_MIN" : 500000-1, # min size of buffer that has to be reached before training
+    "BUFFER_SIZE" : 250000, # max size of buffer
+    "BUFFER_MIN" : 250000-1, # min size of buffer that has to be reached before training
     "KEEP_IN_MEM" : False, # if buffer elements should be kept in memory. If false, samples are stored and retrieved from BUFFER_PATH
     "BUFFER_PATH" : "./buffer/", # path to where the buffer data should be stored
-    "USE_PRIORITIZED_REPLAY" : False, # should prioritized replay be used
-    "EPSILON_DECAY" : 0.004, # how much to decay epsilon each iteration
+    "USE_PRIORITIZED_REPLAY" : True, # should prioritized replay be used
+    "EPSILON_DECAY" : 0.002, # how much to decay epsilon each iteration
     "INNER_ITS" : 20, # how many training steps per iteration
-    "SAMPLES_FROM_ENV" : 4000, # how many new samples from the environment should be added to the buffer each iteration (in expectation)
+    "SAMPLES_FROM_ENV" : 5000, # how many new samples from the environment should be added to the buffer each iteration (in expectation)
     "TRAIN_ITS" : 12000, # how many training iterations should be done
-    "INITIAL_EPSILON" : 0.05, # initial value of epsilon
-    "EPSILON_MIN" : 0.05, # minimum value of epsilon that can be reached
+    "INITIAL_EPSILON" : 1, # initial value of epsilon
+    "EPSILON_MIN" : 0.1, # minimum value of epsilon that can be reached
     "BATCH_SIZE" : 512, # batch size
     "POLYAK_UPDATE" : 0.0008, # polyak update for each training step (so INNER_ITS polyak updates per training iteration)
-    "LEARNING_RATE" : 0.000025, # learning rate for the adam
+    "LEARNING_RATE" : 0.0002, # learning rate for the adam
     "ENV" : "ALE/Asterix-v5", # environment name
-    "LOG_PATH_WEIGHTS" : 'asterix_test/run18/', # where to store the weights
-    "LOG_PATH_TENSORBOARD" : 'logs/asterix_test/run18/', # where to store dqn loss and reward for tensorboard
-    "PRELOAD_WEIGHTS" : 'asterix_test/run8/' # path to preloaded weights
+    "LOG_PATH_WEIGHTS" : 'asterix_test/run30/', # where to store the weights
+    "LOG_PATH_TENSORBOARD" : 'logs/asterix_test/run30/', # where to store dqn loss and reward for tensorboard
+    "PRELOAD_WEIGHTS" : None # path to preloaded weights
     }
 
     # send hyperparameters to the buffer
@@ -51,10 +52,11 @@ if __name__ == '__main__':
     if params["PRELOAD_WEIGHTS"]:
         model.load_weights(params["PRELOAD_WEIGHTS"] + "model")
 
-    
-    for i in range(500):
-        print(i)
-        data = create_trajectory(model, params["INITIAL_EPSILON"], params["ENV"], 4,84,84,1000)
+    total = 0
+    while total < params["BUFFER_MIN"]:
+        print(total)
+        data = create_trajectory(model,128, params["INITIAL_EPSILON"], params["ENV"], 4,84,84)
+        total += len(data)
         soc = socket.socket()
         connected = False
         while not connected:
