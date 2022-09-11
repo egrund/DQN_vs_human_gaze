a62170a5b92feeebfca9c28e8aec9160dff8ef73
egrund <82370915+egrund@users.noreply.github.com>
2022-09-11 13:21:26 +0200

    Update use_integrated_gradients.py

--- saliency/use_integrated_gradients.py
+++ saliency/use_integrated_gradients.py
@@ -1,7 +1,7 @@
 import integrated_gradients as ig 
 from my_reader_class import Reader
 from sample_trajectory import preprocess_image
-import dqn
+from model import AgentModel
 
 import tensorflow as tf
 import numpy as np
@@ -12,12 +12,12 @@ MODE = 'Black'
 M_STEPS = 10
 I = 200 # index of frame (1 to data.get_number_frames())
 
-data = Reader() #file_dir = , images_dir = ) # add path of txt file and 
-model = dqn.model(9)
-model(tf.random.uniform(shape=(1,84,84,4)))
-model.load_weights('asterix_test/run2/model') # add path
+data = Reader(file_dir = "D:/Documents/Gaze_Data_Project/asterix/160_RZ_9166697_Feb-20-16-46-45.txt", images_dir = "D:/Documents/Gaze_Data_Project/asterix/160_RZ_9166697_Feb-20-16-46-45_extracted/")
+model = AgentModel(9)
+model.load_weights('asterix_test/run8/model') # add path
 
-image = preprocess_image(tf.convert_to_tensor(data.get_image(I)),84,84)
+original_image = data.get_image(I)
+image = preprocess_image(tf.convert_to_tensor(original_image),84,84)
 observation = tf.repeat(image,FRAME_SKIPS,axis=-1) # model gets several times the same image
 
 # use action model decides as right or use human decision as right one
@@ -48,7 +48,7 @@ axs[0,0].imshow(baseline_image)
 axs[0,0].axis('off')
 
 axs[0,1].set_title('Original Image')
-axs[0,1].imshow(image, cmap = 'gray')
+axs[0,1].imshow(original_image, cmap = 'gray')
 axs[0,1].axis('off') 
 
 axs[1,0].set_title('IG Attribution Mask')
