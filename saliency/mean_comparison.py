61f1b4e53aee577aa2e17a6eee5067ee5d100a9f
egrund <82370915+egrund@users.noreply.github.com>
2022-09-17 12:54:30 +0200

    mean, 4 gaze
    also 4 gaze samples for mean_comparison

--- saliency/mean_comparison.py
+++ saliency/mean_comparison.py
@@ -6,31 +6,40 @@ import numpy as np
 import random as rand
 import time
 
+# choose from which episode to use the data
+episodes = ["160_RZ_9166697_Feb-20-16-46-45","167_JAW_2356024_Mar-29-15-42-54"]
+EPISODE = 0
+# choose which samples to use from the episode (make sure you have saved the saliency maps for all of them)
+START = 0
+LAST = 10 # have 2000
+STEP = 1
+# you need to at least compare 2 sample to not end in and endless loop in the last part !
+
+FRAME_SKIPS = 4 # How many frames of gaze data to compare to the saliency map
+
 print()
 print("Test Mean")
 print("------------------")
 print()
 
-episodes = ["160_RZ_9166697_Feb-20-16-46-45","167_JAW_2356024_Mar-29-15-42-54"]
-EPISODE = 1
 rand.seed(42)
 
 data = Reader(file_dir = "D:/Documents/Gaze_Data_Project/asterix/" + episodes[EPISODE] +".txt", images_dir = "D:/Documents/Gaze_Data_Project/asterix/" + episodes[EPISODE] +"_extracted/")
 
-START = 0
-LAST = 2000 # have 2000
-STEP = 1
-
 start = time.time()
 
 saliencies = pert.load_saliency(START,LAST,"D:/Documents/Gaze_Data_Project/saliency_database/" + episodes[EPISODE] +"/run8/",STEP)
+gaze_lists = [data.get_gaze(i,FRAME_SKIPS) for i in range(START,LAST+1,STEP)]
+data_maps = [ np.array(gaze_list) for gaze_list in gaze_lists]
+heatmaps = [data.create_gaze_heatmap(i,FRAME_SKIPS) for i in range(START,LAST+1,STEP)]
 
 av_distance = []
 av_middle = []
 for i in range(START,LAST+1,STEP):
-    if not data.get_gaze(i): # if the human did not look anywhere
+    ix = int(i/STEP)
+    if not gaze_lists[ix]: # if the human did not look anywhere
         continue
-    gaze_list = np.array(data.get_gaze(i))
+    gaze_list = data_maps[ix]
     saliency = saliencies[int(i/STEP)]
     dif, dif_norm = compare.compare_by_mean(gaze_list, saliency)
     av_distance.append(dif)
@@ -45,9 +54,10 @@ print("Randomly assigned saliency")
 av_distance = []
 av_middle = []
 for i in range(START,LAST+1,STEP):
-    if not data.get_gaze(i): # if the human did not look anywhere
+    ix = int(i/STEP)
+    if not gaze_lists[ix]: # if the human did not look anywhere
         continue
-    gaze_list = np.array(data.get_gaze(i))
+    gaze_list = data_maps[ix]
     image_index = rand.randrange(START,LAST+1,STEP)
     saliency = saliencies[int(image_index/STEP)]
     dif, dif_norm = compare.compare_by_mean(gaze_list, saliency)
@@ -62,13 +72,15 @@ print("Randomly assigned gaze heatmap")
 av_distance = []
 av_middle = []
 for i in range(START,LAST+1,STEP):
-    if not data.get_gaze(i): # if the human did not look anywhere
+    ix = int(i/STEP)
+    if not gaze_lists[ix]: # if the human did not look anywhere
         continue
-    gaze_list = np.array(data.get_gaze(i))
+    gaze_list = data_maps[ix]
     image_index = rand.randrange(START,LAST+1,STEP)
-    while not data.get_gaze(image_index) or i == image_index:
+    while not gaze_lists[ix] or i == image_index:
         image_index = rand.randrange(START,LAST+1,STEP)
-    saliency = data.create_gaze_heatmap(image_index)
+
+    saliency = heatmaps[ix]
     dif, dif_norm = compare.compare_by_mean(gaze_list, saliency)
     av_distance.append(dif)
     av_middle.append(dif_norm)
