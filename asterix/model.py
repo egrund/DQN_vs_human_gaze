22d7d6e32197589d4592bf677a3209f65478cac1
leonl42 <55513915+leonl42@users.noreply.github.com>
2022-09-19 11:33:42 +0200

    Update model.py

--- asterix/model.py
+++ asterix/model.py
@@ -10,7 +10,7 @@ class AgentModel(Model):
         self._mse = tf.keras.losses.MeanSquaredError()
 
         #self._resize = tf.keras.layers.Resizing(84,84,interpolation='bilinear',crop_to_aspect_ratio=False,)
-        self._l1 = tf.keras.layers.Conv2D(64, (8, 8), strides = (2,2), activation='relu', input_shape=input_shape)#, kernel_regularizer = tf.keras.regularizers.L1L2(l1=0.1, l2=0.1))
+        self._l1 = tf.keras.layers.Conv2D(64, (8, 8), strides = (2,2),activation='relu', input_shape=input_shape)#, kernel_regularizer = tf.keras.regularizers.L1L2(l1=0.1, l2=0.1))
         self._l2 = tf.keras.layers.MaxPooling2D((3, 3))
         self._l3 = tf.keras.layers.Conv2D(64, (4, 4), activation='relu')#, kernel_regularizer = tf.keras.regularizers.L1L2(l1=0.1, l2=0.1))
         self._l4 = tf.keras.layers.MaxPooling2D((3, 3))
@@ -27,23 +27,9 @@ class AgentModel(Model):
 
     @tf.function
     def call(self,x ,training):
-
-        x = tf.cast(x, tf.float32)
+        x = tf.cast(x,tf.float32)
         x = (x-127.5)/127.5
 
-        x = tf.nn.conv2d(x, filters = [[[[0.299,0.0,0.0,0.0],
-        [0.587,0.0,0.0,0.0],
-        [0.114,0.0,0.0,0.0],
-        [0.0,0.299,0.0,0.0],
-        [0.0,0.587,0.0,0.0],
-        [0.0,0.114,0.0,0.0],
-        [0.0,0.0,0.299,0.0],
-        [0.0,0.0,0.587,0.0],
-        [0.0,0.0,0.114,0.0],
-        [0.0,0.0,0.0,0.299],
-        [0.0,0.0,0.0,0.587],
-        [0.0,0.0,0.0,0.114]]]],strides = 1,padding = "VALID")
-        
         x = self._l1(x,training=training)
         x = self._l2(x,training=training)
         x = self._l3(x,training=training)
