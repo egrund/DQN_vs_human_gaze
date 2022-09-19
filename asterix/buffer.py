6cce68d22454334d59bce86842b2b61e279f41ef
leonl42 <55513915+leonl42@users.noreply.github.com>
2022-09-19 11:32:34 +0200

    Update buffer.py

--- asterix/buffer.py
+++ asterix/buffer.py
@@ -1,9 +1,9 @@
 from multiprocessing.managers import SyncManager
 import joblib
-import os
 import random as rand
 import numpy as np
 from pathlib import Path
+
 class Buffer:
 
     def __init__(self, max_buffer_size, min_buffer_size, buffer_path, keep_in_mem, use_prioritized_replay) -> None:
@@ -32,6 +32,7 @@ class Buffer:
         if self.use_prioritized_replay and not len(self.data.keys()) < self._max_buffer_size:
             keys = list(self.priorities.keys())
             priorities = list(self.priorities.values())
+            max_priority = max(priorities)
             for sars, index in zip(data, [int(key) for key,_ in sorted(zip(keys,priorities), key = lambda item : item[1])]):
 
                 self.at_file = index
@@ -43,13 +44,12 @@ class Buffer:
                     with open( self.buffer_path + str(int(self.at_file/10000)) + "/" + str(self.at_file) + ".pkl", "wb" ) as f:
                         joblib.dump(sars,f)
 
-                self.priorities[str(self.at_file)] = max(priorities)
+                self.priorities[str(self.at_file)] = max_priority
                 
         else:
 
             for sars in data:
 
-                priorities = list(self.priorities.values())
                 if self.keep_in_mem:
                     self.data[str(self.at_file)] = sars
                 else:
@@ -57,10 +57,9 @@ class Buffer:
                     with open( self.buffer_path + str(int(self.at_file/10000)) + "/" + str(self.at_file) + ".pkl", "wb" ) as f:
                         joblib.dump(sars,f)
 
-                if len(priorities) == 0:
-                    self.priorities[str(self.at_file)] = 1000
-                else:
-                    self.priorities[str(self.at_file)] = max(priorities)
+
+                self.priorities[str(self.at_file)] = 100
+
 
                 self.at_file += 1
                 if self.at_file > self._max_buffer_size:
@@ -69,10 +68,12 @@ class Buffer:
     def get_indices(self, batch_size):
 
         if self.use_prioritized_replay:
-            priorities = list(self.priorities.values())
-            priorities = [priority**0.3 for priority in priorities]
-            priorities_sum = (sum(priorities,start=0)+1)
-            priorities = [priority/priorities_sum for priority in priorities]
+            priorities = np.array(list(self.priorities.values()))
+            priorities = np.power(priorities,0.3)
+            priorities = priorities/(np.sum(priorities)+1)
+            #priorities = [priority**0.3 for priority in priorities]
+            #priorities_sum = (sum(priorities,start=0)+1)
+            #priorities = [priority/priorities_sum for priority in priorities]
 
             return rand.choices([int(key) for key in self.priorities.keys()], weights=priorities,k = batch_size)
 
@@ -90,17 +91,17 @@ class Buffer:
             else:
                 with open( "./buffer/" + str(int(index/10000)) + "/" + str(index) + ".pkl", "rb" ) as f:
                     s_a_r_s.append(joblib.load(f))
-        
-        s = np.stack([elem[0] for elem in s_a_r_s],axis = 0)
-        a = np.stack([elem[1] for elem in s_a_r_s],axis = 0)
-        r = np.stack([elem[2] for elem in s_a_r_s],axis = 0)
-        s_new = np.stack([elem[3] for elem in s_a_r_s],axis = 0)
-        done = np.stack([elem[4] for elem in s_a_r_s],axis = 0)
+
+        s = [elem[0] for elem in s_a_r_s]
+        a = [elem[1] for elem in s_a_r_s]
+        r = [elem[2] for elem in s_a_r_s]
+        s_new = [elem[3]  for elem in s_a_r_s]
+        done = [elem[4] for elem in s_a_r_s]
 
         return {"values" : (s,a,r,s_new,done), "indices" : indices}
 
 
-    def update_priority(self,priorities):
+    def update_priorities(self,priorities):
 
         for key, value in priorities.items():
             self.priorities[key] = value
