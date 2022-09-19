005126e9e9764f82189a663a46c60c62aedb77f1
leonl42 <55513915+leonl42@users.noreply.github.com>
2022-09-19 11:32:56 +0200

    Update dqn.py

--- asterix/dqn.py
+++ asterix/dqn.py
@@ -1,21 +1,13 @@
-import threading
 import numpy as np
-import queue
 import datetime
 import gym
-import socket
-from sample_trajectory_client import create_trajectory_client_send,create_trajectory_client_receive
+from sample_trajectory import create_trajectory
 import time
 import joblib
-import pickle
 import tensorflow as tf
-import cProfile
-import io 
-import pstats
-
 class agent:
     import tensorflow as tf
-    def __init__(self, use_prioritized_replay : bool , env : str,epsilon : int,epsilon_decay : float, epsilon_min : float, batch_size : int,learning_rate :float,inner_its : int, samples_from_env : int,polyak_update : float, preload_weights_path : str):
+    def __init__(self, buffer, use_prioritized_replay : bool , env : str,epsilon : int,epsilon_decay : float, epsilon_min : float, batch_size : int,learning_rate :float,inner_its : int, samples_from_env : int,polyak_update : float, preload_weights_path : str):
         from model import AgentModel
         import tensorflow as tf
         self.inner_its = inner_its
@@ -29,14 +21,15 @@ class agent:
         self.epsilon_min = epsilon_min
         self.use_prioritized_replay = use_prioritized_replay
         self.sample_correction = 1
+        self.buffer = buffer
 
         
         self.model = AgentModel(gym.make(self.env,full_action_space=False,new_step_api=True).action_space.n)
         self.model_target = AgentModel(gym.make(self.env,full_action_space=False,new_step_api=True).action_space.n)
 
         # initialize weights 
-        self.model(tf.random.uniform(shape=(1,84,84,12)))
-        self.model_target(tf.random.uniform(shape=(1,84,84,12)))
+        self.model(tf.random.uniform(shape=(1,84,84,4)))
+        self.model_target(tf.random.uniform(shape=(1,84,84,4)))
 
         if preload_weights_path:
             self.model.load_weights(preload_weights_path + "model")
@@ -63,56 +56,59 @@ class agent:
         time_buffer_update_writer = tf.summary.create_file_writer(time_buffer_update_log_dir)
 
         current_epsilon = self.epsilon
+        
         for i in range(its):
-
+            #print("buffer size in gb: ", asizeof.asizeof(self.buffer)/1000000000)
+            #print("agent size in gb: ", asizeof.asizeof(self)/1000000000)
             # update epsilon
             if current_epsilon > self.epsilon_min:
                 current_epsilon -= self.epsilon_decay
 
-
-            ##################################################
-            # initiate trajectory sampling
-            
-            soc = socket.socket()
-            connected = False
-            while not connected:
-                try: 
-                    soc.connect(('localhost',7995))
-                    connected = True
-                except:
-                    print("Could not connect to localhost on port" , 7995)
-            with soc:
-                soc.sendall(pickle.dumps({"weights" : self.model.get_weights(), "batch" : int(32*self.sample_correction)+1, "epsilon" : current_epsilon, "env_name" : self.env, "frame_skips" : 4, "imgx" : 84, "imgy" : 84}))
-               
             ##################################################
             # train 
-
+            #memory_tracker = tracker.SummaryTracker()
             start_time_step = time.time()
-            self.perform_training(None,None)
-            self.perform_training(None,None)
-            self.perform_training(None,None)
-            self.perform_training(None,None)
-            self.perform_training(None,None)
+            #profiler = cProfile.Profile()
+            #profiler.enable()
             self.perform_training(dqn_summary_writer,i)
+            
+            #profiler.disable()
+            #s = io.StringIO()
+            #ps = pstats.Stats(profiler, stream=s).sort_stats('tottime')
+            #ps.print_stats()
+            #with open('it_logs/it' + str(i) + 'training.txt', 'w+') as f:
+            #    f.write(s.getvalue())
             end_time_step = time.time()
+
+            #memory_tracker.print_diff()
 
             #############################################################
             # retrieve trajectory samples
 
             start_time_sample = time.time()
-            q_new_samples = queue.Queue()
-            self.perform_sampling(reward_summary_writer,sample_correction_writer,current_epsilon,i, q_new_samples)
-            new_data = q_new_samples.get(block=True)
+            #profiler = cProfile.Profile()
+            #profiler.enable()
+            new_data = self.perform_sampling(reward_summary_writer,sample_correction_writer,current_epsilon,i)
+            #profiler.disable()
+            #s = io.StringIO()
+            #ps = pstats.Stats(profiler, stream=s).sort_stats('tottime')
+            #ps.print_stats()
+            #with open('it_logs/it' + str(i) + 'sampling.txt', 'w+') as f:
+            #    f.write(s.getvalue())
+
             end_time_sample = time.time()
 
             #############################################################
             # add new data to the buffer
 
+            #memory_tracker.print_diff()
+
             start_time_buffer = time.time()
-            self.update_buffer(new_data)
+            self.buffer.extend(new_data)
             end_time_buffer = time.time()
 
-            
+            #memory_tracker.print_diff()
+
             with time_environment_sample_summary_writer.as_default():
                 tf.summary.scalar("time_environment", end_time_sample - start_time_sample, step = i*self.inner_its)
 
@@ -131,68 +127,10 @@ class agent:
             
             print("round: ", i , " gpu memory: ", tf.config.experimental.get_memory_info("/GPU:0"))
 
-    def update_buffer(self,data):
-        soc = socket.socket()
-        connected = False
-        while not connected:
-            try: 
-                soc.connect(('localhost',7999))
-                connected = True
-            except:
-                pass
-        with soc:
-            soc.sendall(pickle.dumps({"task" : "extend", "values" : data}))
-
-    def get_minibatch(self, port, queue):
-        fragments = []
-
-        soc = socket.socket()
-        connected = False
-        while not connected:
-            try: 
-                soc.connect(('localhost',port))
-                connected = True
-            except:
-                print("Could not connect to " , port)
-        fragments = []
-        with soc:
-            while True:
-                recvfile = soc.recv(4096)
-                if not recvfile: break
-                fragments.append(recvfile)
-        queue.put(pickle.loads(b''.join(fragments)))
-    
-    def update_priorities(self,td):
-        soc = socket.socket()
-        connected = False
-        while not connected:
-            try: 
-                soc.connect(('localhost',7999))
-                connected = True
-            except:
-                print("Could not connect to 7999")
-        with soc:
-            soc.sendall(pickle.dumps({"task" : "priority", "values" : td}))
-
-    def perform_sampling(self,reward_summary_writer,sample_correction_writer,current_epsilon,i, q):
+    def perform_sampling(self,reward_summary_writer,sample_correction_writer,current_epsilon,i):
         
         # sample new trajectory
-        fragments = []
-        soc = socket.socket()
-        connected = False
-        while not connected:
-            try: 
-                soc.connect(('localhost',7995))
-                connected = True
-            except:
-                print("Could not connect to localhost on port" , 7995)
-        fragments = []
-        with soc:
-            while True:
-                recvfile = soc.recv(4096)
-                if not recvfile: break
-                fragments.append(recvfile)
-        new_data = pickle.loads(b''.join(fragments))
+        new_data = create_trajectory(self.model,int(18*self.sample_correction)+1, current_epsilon,self.env, 4,84,84)
 
         self.sample_correction = (self.samples_from_env/len(new_data))*self.sample_correction
 
@@ -202,77 +140,53 @@ class agent:
 
         # log average reward of average reward in tensorboard
         with reward_summary_writer.as_default():
-            tf.summary.scalar("reward", tf.reduce_mean(reward), step = i*self.inner_its)
+            tf.summary.scalar("reward", tf.reduce_sum(reward), step = i*self.inner_its)
 
         with sample_correction_writer.as_default():
             tf.summary.scalar("sample_correction", self.sample_correction, step = i*self.inner_its)
+    
         
-        
-        
-        q.put(new_data)
+        return new_data
+    
+    @tf.function
+    def calculate_td_error(self, s, a, r,s_new):
+
+        a_new = tf.argmax(self.model(s_new,training = False),axis = 1)
+        old_q_value = tf.gather(self.model(s,training=False),tf.cast(a,dtype=tf.int32),batch_dims=1)
+        new_q_value = tf.gather(self.model_target(s,training=False),a_new, batch_dims=1)
+
+        TD_error = tf.abs(r + tf.constant(0.99) * new_q_value - old_q_value)
+        return TD_error
 
     def perform_training(self,dqn_summary_writer,i):
 
-        data_queue = queue.Queue()
-        td = {}
 
-        soc = socket.socket()
-        connected = False
-        while not connected:
-            try: 
-                soc.connect(('localhost',7997))
-                connected = True
-            except:
-                print("Could not connect to 7997")
-        with soc:
-            # preload some samples
-            threading.Thread(target = self.get_minibatch, args = (8000,data_queue)).start()
-            threading.Thread(target = self.get_minibatch, args = (8001,data_queue)).start()
-            threading.Thread(target = self.get_minibatch, args = (8002,data_queue)).start()
-            threading.Thread(target = self.get_minibatch, args = (8003,data_queue)).start()
-            threading.Thread(target = self.get_minibatch, args = (8004,data_queue)).start()
-            threading.Thread(target = self.get_minibatch, args = (8005,data_queue)).start()
-            threading.Thread(target = self.get_minibatch, args = (8006,data_queue)).start()
-            threading.Thread(target = self.get_minibatch, args = (8007,data_queue)).start()
 
-            for j in range(self.inner_its): 
+       
+        for j in range(self.inner_its): 
+            td = {}
 
-                with tf.device("/CPU:0"):
-                    
-                    if j < self.inner_its-8:
-                        threading.Thread(target = self.get_minibatch, args = (8000+j+8,data_queue)).start()
+            with tf.device("/CPU:0"):
+                
+                training_data = self.buffer.sample_minibatch(self.buffer.get_indices(self.batch_size))
+                s,a,r,s_new,done = training_data["values"]
+                indices = training_data["indices"]
 
-                    new_data = data_queue.get(block = True)
-                    s,a,r,s_new,done = new_data["values"]
-                    indices = new_data["indices"]
+                a = tf.cast(a,tf.float32)
+                r = tf.cast(r,tf.float32)
+                done = tf.cast(done,tf.float32)
 
-                    s = tf.cast(s,tf.float32)
-                    a = tf.cast(a,tf.float32)
-                    r = tf.cast(r,tf.float32)
-                    s_new = tf.cast(s_new,tf.float32)
-                    done = tf.cast(done,tf.float32)
-
-                with tf.device("/GPU:0"):
-                    loss = self.model.step(s,a,r,s_new,done, self.optimizer, self.model_target)
-
-                    if self.use_prioritized_replay:
-                        a_new = tf.argmax(self.model(s_new,training = False),axis = 1)
-                        old_q_value = tf.gather(self.model(s,training=False),tf.cast(a,dtype=tf.int32),batch_dims=1)
-                        new_q_value = tf.gather(self.model_target(s,training=False),a_new, batch_dims=1)
-
-                        TD_error = tf.abs(r + tf.constant(0.99) * new_q_value - old_q_value)
-                        TD_error = TD_error.numpy()
-
+            with tf.device("/GPU:0"):
+                loss = self.model.step(s,a,r,s_new,done, self.optimizer, self.model_target)
 
                 if self.use_prioritized_replay:
-                    for m,index in enumerate(indices):
-                        td[str(index)] = TD_error[m]
+                    TD_error = self.calculate_td_error(s,a,r,s_new).numpy()
 
-        
-        if self.use_prioritized_replay:
-            print("max: ", max(list(td.values())))
-            print("min: ", min(list(td.values())))
-            self.update_priorities(td)
+
+            if self.use_prioritized_replay:
+                for m,index in enumerate(indices):
+                    td[str(index)] = TD_error[m]
+                self.buffer.update_priorities(td)
 
         # apply polyak averaging
         self.model_target.set_weights((1-self.polyak_update)*np.array(self.model_target.get_weights(),dtype = object) + self.polyak_update*np.array(self.model.get_weights(),dtype = object))
